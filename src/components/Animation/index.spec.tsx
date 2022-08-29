b313af3da96f953757e0f7cf2ca6a3f55e284484
jay <>
2022-08-29 11:18:55 +0530

    Animation component testcase added

--- src/components/Animation/index.spec.tsx
+++ src/components/Animation/index.spec.tsx
@@ -1,10 +1,8 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Sample from './Sample';
-// import Animation from './index';
+import Animation from './index';
+import { renderWithProviders } from '../../test/test-utils';
 
 describe('Animation', function () {
   it('should Render the Animation Component', () => {
-    render(<Sample />);
-    fireEvent.click(screen.getByText('Increament'));
+    renderWithProviders(<Animation />);
   });
 });
