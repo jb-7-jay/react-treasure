fb75abd20c6e557d67d41fdd00b672cd6bb6b0cf
jay <>
2022-09-02 01:45:02 +0530

    Mulish font updated

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -9,19 +9,22 @@ import StickyFooterButton from '../common/StickyFooterButton';
 import AppRoutes from '../../AppRoutes';
 
 import '../../index.css';
+import './index.scss';
 
 const persistor = persistStore(store);
 
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <AppRoutes />
-          <StickyFooterButton />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <div className="App">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <AppRoutes />
+            <StickyFooterButton />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </div>
   );
 }
 
